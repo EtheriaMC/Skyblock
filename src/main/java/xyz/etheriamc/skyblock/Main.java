59de3c6fbae56f3097603d37a92c508d6f0267ee
Vifez <vifez01@gmail.com>
2024-07-24 22:21:01 +0100

    Added scoreboard + PlaceholderAPI Support

--- src/main/java/xyz/etheriamc/skyblock/Main.java
+++ src/main/java/xyz/etheriamc/skyblock/Main.java
@@ -1,40 +1,50 @@
 package xyz.etheriamc.skyblock;
 
-import co.aikar.commands.PaperCommandManager;
-import lombok.Getter;
+import io.github.thatkawaiisam.assemble.Assemble;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.WorldType;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
-import xyz.etheriamc.skyblock.commands.ACFResolver;
+import xyz.etheriamc.skyblock.commands.CommandHandler;
 import xyz.etheriamc.skyblock.listeners.EventListener;
 import xyz.etheriamc.skyblock.managers.IslandManager;
+import xyz.etheriamc.skyblock.util.ConfigFile;
+import xyz.etheriamc.skyblock.util.adapters.BoardAdapter;
 
 import java.util.Random;
 
-@Getter
 public class Main extends JavaPlugin {
-    @Getter private static Main instance;
     private IslandManager islandManager;
     private World islandWorld;
-    private PaperCommandManager paperCommandManager;
+
+    private ConfigFile scoreboardFile;
 
     @Override
     public void onEnable() {
-        instance = this;
+        loadOthers();
+        loadFiles();
         createVoidWorld();
         islandManager = new IslandManager(this, islandWorld);
-        paperCommandManager = new PaperCommandManager(this);
-        ACFResolver.registerAll();
+
+        getCommand("is").setExecutor(new CommandHandler(islandManager));
 
         getServer().getPluginManager().registerEvents(new EventListener(islandManager), this);
     }
 
+    private void loadFiles() {
+        saveDefaultConfig();
+
+        this.scoreboardFile = new ConfigFile(this, "scoreboard.yml");
+    }
+
+    private void loadOthers() {
+        new Assemble(this, new BoardAdapter(this));
+    }
+
     @Override
     public void onDisable() {
-        instance = null;
         islandManager.saveIslands();
     }
 
@@ -51,6 +61,10 @@ public class Main extends JavaPlugin {
 
     public IslandManager getIslandManager() {
         return islandManager;
+    }
+
+    public ConfigFile getScoreboardFile() {
+        return scoreboardFile;
     }
 
     public class VoidWorldGenerator extends ChunkGenerator {
